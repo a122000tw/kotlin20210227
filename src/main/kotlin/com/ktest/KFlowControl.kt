a8badfdfed19093d7fb1de204687be0cd364604d
MB-study <a122000tw@gmail.com>
2021-02-27 16:12:18 +0800

    Kotlin

--- src/main/kotlin/com/ktest/KFlowControl.kt
+++ src/main/kotlin/com/ktest/KFlowControl.kt
@@ -1,17 +1,27 @@
 package com.ktest
 
-import kotlin.random.Random
+import java.util.*
+import kotlin.random.Random as r
 
 
 class KFlowControl {
 }
 
+fun getLevel(score : Int): String = when(score) {
+    in 90..100 -> "A"
+    in 80..89 -> "B"
+    in 70..79 -> "C"
+    in 60..69 -> "D"
+    else -> "E"
+}
+
 // if, when, for, while, do while
 fun main(args: Array<String>) {
     // if
-    var score = Random.nextInt(101)
+    var score = r.nextInt(101)
     val pass = if(score >= 60) "Pass" else "Fail"
     println("$score $pass")
+
     // 100~90 "A" 89~80 "B" 79~70 "C" 69~60 "D" 59~0 "E"
     // when
     when(score) {
@@ -28,6 +38,7 @@ fun main(args: Array<String>) {
         in 60..69 -> "D"
         else -> "E"
     }
+    println(level)
     level = when(score/10) {
         9, 10 -> "A"
         8 -> "B"
@@ -36,7 +47,47 @@ fun main(args: Array<String>) {
         else -> "E"
     }
     println(level)
+    println(getLevel(score))
 
+    // for
+    for(i in 1..10) {
+        print("$i ")
+    }
+    println()
+
+    for(i in 1..10 step 2) {
+        print("$i ")
+    }
+    println()
+
+    for(i in 10 downTo 1) {
+        print("$i ")
+    }
+    println()
+
+    for(i in 1 until 10) {
+        print("$i ")
+    }
+    println()
+
+    // 動動腦
+    // 有一(1~10)亂數 ans 使用者來猜, 猜對Bingo, 有五次機會
+    // 加入請猜小/大一點的提示
+    val ans = r.nextInt(10)+1
+    val max = 5
+    for(i in 1..max) {
+        print("第 $i/$max 次: 請輸入數字: ")
+        // val guess = Scanner(System.`in`).nextInt()
+        val guess = readLine()!!.toInt()
+        if(guess == ans) {
+            println("Bingo")
+            break
+        } else if (guess < ans) {
+            println("請猜大一點")
+        } else if(guess > ans) {
+            println("請猜小一點")
+        }
+    }
 
 
 }
