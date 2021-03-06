dc0014b502cda4b8d6e6935d0875409ee28d991b
MB-study <a122000tw@gmail.com>
2021-03-06 13:41:18 +0800

    Kotlin

--- src/main/kotlin/com/ktest/Wallet.kt
+++ src/main/kotlin/com/ktest/Wallet.kt
@@ -1,16 +1,28 @@
 package com.ktest
-    // 錢包
-data class Wallet(val name: String, var money: Int ) {
+
+import java.lang.Exception
+
+// 錢包
+data class Wallet(val name: String, var money: Int) {
     // 如何檢查傳進來的參數是否正確?
     init {
-        money = if(money > 0) money else 0
+        // case1
+        //money = if(money > 0) money else 0
+        // case2
+        money = try {
+            // require檢驗函式, 檢驗失敗會拋出例外
+            require(money >= 0)
+            money
+        } catch (e: Exception) {
+            0
+        }
     }
 }
 
 fun main() {
-    val w1 = Wallet("John",100)
+    val w1 = Wallet("John", 100)
     println(w1)
-    val w2 = Wallet("Mary",-100)
+    val w2 = Wallet("Mary", -100)
     println(w2)
 
 }
