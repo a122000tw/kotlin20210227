916ef6462d7a3c975d192232550a8aabfc338da2
MB-study <a122000tw@gmail.com>
2021-03-20 14:05:59 +0800

    Kotlin

---
+++ src/main/kotlin/com/進階控制/檔案存取/KFileAccess.kt
@@ -0,0 +1,27 @@
+package com.進階控制.檔案存取
+
+import java.io.File
+
+// 讀取檔案資料
+fun read() {
+    File("data.txt").bufferedReader().lines().forEach{
+        println(it)
+    }
+}
+// 寫入資料
+fun write(data: String) {
+    File("data.txt").appendText("$data \n")
+}
+
+// 寫入資料(取代原檔案內容)
+fun replace(data: String) {
+    File("data.txt").writeText("$data \n")
+}
+
+fun main() {
+    read()
+    write("VB")
+    read()
+    replace("Cobol")
+    read()
+}
