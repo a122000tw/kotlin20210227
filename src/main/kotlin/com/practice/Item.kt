8d3ae327b5da1585515ec35dccaf0c3864bd66c8
a122000tw <a122000tw@gmail.com>
2021-11-25 18:37:01 +0800

    123

--- src/main/kotlin/com/practice/Item.kt
+++ src/main/kotlin/com/practice/Item.kt
@@ -1,5 +1,7 @@
 package com.practice
 
+import kotlin.collections.List
+
 open class Item(val name: String, val price: Int) {
 }
 
@@ -25,10 +27,60 @@ class Vegetables(vararg val toppings: String): Item("Vegetables", 5) {
 }
 
 fun main() {
-    val noodles = Noodles()
-    val vegetables = Vegetables("Cabbage", "Sprouts", "Onion")
-    val vegetables2 = Vegetables()
-    println(noodles)
-    println(vegetables)
-    println(vegetables2)
+//    val noodles = Noodles()
+//    val vegetables = Vegetables("Cabbage", "Sprouts", "Onion")
+//    val vegetables2 = Vegetables()
+//    println(noodles)
+//    println(vegetables)
+//    println(vegetables2)
+    val ordersList = mutableListOf<Order>()
+
+    val order1 = Order(1)
+    order1.addItem(Noodles())
+    ordersList.add(order1)
+
+    val order2 = Order(2)
+    order2.addItem(Noodles())
+    order2.addItem(Vegetables())
+    ordersList.add(order2)
+
+    val order3 = Order(3)
+    val items = listOf(Noodles(), Vegetables("Carrots", "Beans", "Celery"))
+    order3.addAll(items)
+    ordersList.add(order3)
+
+    val order4 = Order(4).addItem(Noodles()).addItem(Vegetables("Cabbage", "Onion"))
+    ordersList.add(order4)
+
+    ordersList.add(Order(5)
+        .addItem(Noodles())
+        .addItem(Noodles()).addItem(Vegetables("Spinach")))
+
+    for (order in ordersList) {
+        order.print()
+        println()
+    }
+}
+
+class Order(val orderNumber: Int) {
+    private val itemList = mutableListOf<Item>()
+
+    fun addItem(newItem: Item): Order {
+        itemList.add(newItem)
+        return this
+    }
+    fun addAll(newItems: List<Item>): Order {
+        itemList.addAll(newItems)
+        return this
+    }
+    fun print() {
+        println("Order #${orderNumber}")
+        var total = 0
+        for (item in itemList) {
+            println("${item}: $${item.price}")
+            total += item.price
+        }
+        println("Total: $${total}")
+    }
+
 }
